c9d0d8d5d5944f6fef0873be1d1f809cc940fe98
Polar Bear <polarbear@Polars-MacBook-Pro.local>
2026-05-03 23:29:27 +0500

    updated log

--- src/components/ui/HomeContent.jsx
+++ src/components/ui/HomeContent.jsx
@@ -1,7 +1,22 @@
 import Hero from "./Hero";
+import VideoBackdrop from "../shared/VideoBackdrop";
+import AboutSection from "./AboutSection";
+import ExperienceSection from "./ExperienceSection";
+import TechStack from "./TechStack";
+import ContactSection from "./ContactSection";
 
 function HomeContent() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <VideoBackdrop>
+        <AboutSection />
+        <ExperienceSection />
+        <TechStack />
+        <ContactSection />
+      </VideoBackdrop>
+    </>
+  );
 }
 
 export default HomeContent;
